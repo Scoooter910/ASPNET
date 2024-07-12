676bb4810f10fce32b308509b1b133c4db9b9012
Michele Mostoller <MMostoller@att.net>
2024-07-12 13:05:20 -0400

    Completed View Product and Update Product

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -8,5 +8,8 @@ namespace Testing
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
     }
 }
